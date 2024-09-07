637b3a4ed4ee0ad8c11c68ce3519821c958380bf
hatimlechgar <h.lechgar@gmail.com>
2024-09-07 04:23:25 +0200

    add name of consultant in skills view

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
                 $('#consultant-list').append(`
                     <li class="list-group-item d-flex justify-content-between align-items-center">
                         ${consultant.firstName} ${consultant.lastName} (CIN: ${consultant.CIN})
-                        <button class="btn btn-primary show-skills-btn" data-id="${consultant._id}">Show Skills</button>
+                        <button class="btn btn-primary show-skills-btn" data-id="${consultant._id}" data-name="${consultant.firstName} ${consultant.lastName}">Show Skills</button>
                     </li>
                 `);
             });
@@ -22,8 +22,10 @@ $(document).ready(function() {
     // Show skills of the selected consultant
     $(document).on('click', '.show-skills-btn', function() {
         const consultantId = $(this).data('id');
+        const consultantName = $(this).data('name');
         $.get(`${BACKEND_CONSULTANTS}/consultants/${consultantId}`, function(consultant) {
             $('#consultant-list').hide();
+            $('#skills-title').text(`Skills of ${consultantName}`);
             $('#skills-list').empty();
             consultant.skills.forEach(skill => {
                 $('#skills-list').append(`<li class="list-group-item">${skill}</li>`);
