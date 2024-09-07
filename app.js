a4cf6715606f27c839fb3fe78479863ffce6f420
hatimlechgar <h.lechgar@gmail.com>
2024-09-07 04:36:26 +0200

    hide back to list button at first load

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-const BACKEND_CONSULTANTS = 'https://back-demo-001-cnacf2c3cta4embv.northeurope-01.azurewebsites.net';
-
 $(document).ready(function() {
+    // Initially hide the back button
+    $('#back-btn').hide();
+
     function loadConsultants() {
         $.get(`${BACKEND_CONSULTANTS}/consultants`, function(data) {
             $('#consultant-list').empty();
@@ -31,14 +32,14 @@ $(document).ready(function() {
                 $('#skills-list').append(`<li class="list-group-item">${skill}</li>`);
             });
             $('#skills-view').show();
-            $('#back-btn').show();
+            $('#back-btn').show();  // Show the back button only when needed
         });
     });
 
     // Back to consultant list
     $('#back-btn').click(function() {
         $('#skills-view').hide();
-        $('#back-btn').hide();
+        $('#back-btn').hide();  // Hide the back button when returning to the list
         loadConsultants();
     });
 
