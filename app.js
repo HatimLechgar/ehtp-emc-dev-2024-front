39b00d95d4b9bc4480d5833712121f95400289fb
hatimlechgar <h.lechgar@gmail.com>
2024-09-07 03:48:21 +0200

    load skills for each consultant

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 $(document).ready(function() {
-    $('#load-btn').click(function() {
-        $.get('https://back-demo-001-cnacf2c3cta4embv.northeurope-01.azurewebsites.net/consultants', function(data) {
+    function loadConsultants() {
+        $.get('http://localhost:3000/consultants', function(data) {
             $('#consultant-list').empty(); // Clear the list before appending
+            $('#skills-view').hide();
+            $('#consultant-list').show();
             data.forEach(consultant => {
                 $('#consultant-list').append(`
-                    <li class="list-group-item">
+                    <li class="list-group-item consultant-item" data-id="${consultant._id}">
                         ${consultant.firstName} ${consultant.lastName} (CIN: ${consultant.CIN})
                     </li>
                 `);
@@ -12,5 +14,29 @@ $(document).ready(function() {
         }).fail(function() {
             alert('Error loading consultants.');
         });
+    }
+
+    // Show skills of the selected consultant
+    $(document).on('click', '.consultant-item', function() {
+        const consultantId = $(this).data('id');
+        $.get(`http://localhost:3000/consultants/${consultantId}`, function(consultant) {
+            $('#consultant-list').hide();
+            $('#skills-list').empty();
+            consultant.skills.forEach(skill => {
+                $('#skills-list').append(`<li class="list-group-item">${skill}</li>`);
+            });
+            $('#skills-view').show();
+            $('#back-btn').show();
+        });
     });
+
+    // Back to consultant list
+    $('#back-btn').click(function() {
+        $('#skills-view').hide();
+        $('#back-btn').hide();
+        loadConsultants();
+    });
+
+    // Load consultants initially
+    loadConsultants();
 });
