edac8cc814c02795be30b071b50f1b656b89da4d
hatimlechgar <h.lechgar@gmail.com>
2024-09-07 05:01:49 +0200

    call BACKEND_INTELL_MISSION_PROPOSER

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const BACKEND_CONSULTANTS = 'https://back-demo-001-cnacf2c3cta4embv.northeurope-01.azurewebsites.net';
+const BACKEND_INTELL_MISSION_PROPOSER = 'https://back-inteligent-mission-proposer-001-cpckegfpdna5c8ah.northeurope-01.azurewebsites.net';
 
+    
 $(document).ready(function() {
-    // Initially hide the back button
-    $('#back-btn').hide();
+    let currentSkills = [];
 
     function loadConsultants() {
         $.get(`${BACKEND_CONSULTANTS}/consultants`, function(data) {
@@ -29,20 +30,38 @@ $(document).ready(function() {
         $.get(`${BACKEND_CONSULTANTS}/consultants/${consultantId}`, function(consultant) {
             $('#consultant-list').hide();
             $('#skills-title').text(`Skills of ${consultantName}`);
+            currentSkills = consultant.skills;
             $('#skills-list').empty();
             consultant.skills.forEach(skill => {
                 $('#skills-list').append(`<li class="list-group-item">${skill}</li>`);
             });
             $('#skills-view').show();
-            $('#back-btn').show();  // Show the back button only when needed
+            $('#back-btn').show();
         });
     });
 
     // Back to consultant list
     $('#back-btn').click(function() {
         $('#skills-view').hide();
-        $('#back-btn').hide();  // Hide the back button when returning to the list
+        $('#back-btn').hide();
         loadConsultants();
+    });
+
+    // Suggest missions based on skills
+    $('#suggest-missions-btn').click(function() {
+        $.post(`${BACKEND_INTELL_MISSION_PROPOSER}/suggest-missions`, { skills: currentSkills }, function(missions) {
+            $('#missions-list').empty();
+            if (missions.length > 0) {
+                missions.forEach(mission => {
+                    $('#missions-list').append(`<li class="list-group-item">${mission.name}</li>`);
+                });
+            } else {
+                $('#missions-list').append(`<li class="list-group-item">No matching missions found</li>`);
+            }
+            $('#missions-view').show();
+        }).fail(function() {
+            alert('Error suggesting missions.');
+        });
     });
 
     // Load consultants initially
